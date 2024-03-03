fbaf7719b2b04b7357224e20abf2c461571891ad
Leopoldo Rosas <lrosas@gmail.com>
2024-03-03 22:44:26 +0100

    init commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,27 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.python3_host_prog="C:\\Users\\Polo\\pyenvs\\nvim\\Scripts\\python.exe"
+
+-- terminal settings
+local powershell_options = {
+  shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+  shellquote = "",
+  shellxquote = "",
+}
+
+for option, value in pairs(powershell_options) do
+  vim.opt[option] = value
+end
+
+vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
+vim.keymap.set('t', '<C-w>', "<C-\\><C-n><C-w>")
+
+-- minimize terminal split
+vim.keymap.set('n', '<C-g>', "3<C-w>_")
+
